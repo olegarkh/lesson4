19975a3f5eb9292c705a175f9146a47ab6c31f53
Oleg <oarch@mail.ru>
2015-03-02 19:26:35 +0300

    класс новостей

---
+++ models/News.php
@@ -0,0 +1,17 @@
+<?php
+
+class News
+{
+   public $id;
+   public $title;
+   public $text;
+
+   public static function getAll(){
+      $db = new DB;
+      return $db->queryAll("SELECT * FROM news",'News');
+   }
+   public static function getOne($id){
+      $db = new DB;
+      return $db->queryOne("SELECT * FROM news WHERE id = '$id' ",'News');
+   }
+}
