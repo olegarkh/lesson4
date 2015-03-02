2fa3e1e38f8b1d60fe4275363575f82f567736a3
Oleg <oarch@mail.ru>
2015-03-02 19:32:27 +0300

    Фронт-контроллер

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo 'Урок 4 <br>';
+require_once __DIR__ . '/settings/autoload.php';
+
+$ctrl = !empty($_GET['ctrl']) ? $_GET['ctrl'] : 'News';
+$act = isset($_GET['act']) ? $_GET['act'] : 'All';
+
+$controllerClassName = $ctrl . 'Controller';
+$controller = new $controllerClassName;
+$method = 'action'.$act;
+
+$controller->$method();
